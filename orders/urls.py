5ff363e2862f5908f6cd60716bf083f75a54d413
Arjav <sanghaviarjav@gmail.com>
2020-05-26 00:32:32 +0800

    Polised models and added sign up, login, and logoff features

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("sign_up", views.sign_up, name="sign_up"),
+    path("sign_up_verification", views.sign_up_verification, name="sign_up_verification")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,40 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    if not request.user.is_authenticated:
+        return render(request, "orders/login.html", {"message" : None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "orders/homepage.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/login.html", {"message" : "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "orders/login.html", {"message": "Logged out."})
+
+def sign_up(request):
+    return render(request, "orders/sign_up.html", {})
+
+def sign_up_verification(request):
+    first_name = request.POST["first_name"]
+    last_name = request.POST["last_name"]
+    email = request.POST["email"]
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+    return render(request, "orders/homepage.html", {"user": user})
